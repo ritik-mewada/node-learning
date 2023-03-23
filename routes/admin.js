1aac110d73d8fc4b49c39004181b040d929fd68f
Ritik Mewada <ritikmewada@gmail.com>
2023-03-23 11:38:10 +0530

    till DB

--- routes/admin.js
+++ routes/admin.js
@@ -7,5 +7,8 @@ router.get("/add-product", adminController.getAllProducts);
 router.get("/products", adminController.getProducts);
 
 router.post("/add-product", adminController.postAllProducts);
+router.get("/edit-product/:productId", adminController.getEditProducts);
 
+router.post("/edit-product", adminController.postEditProduct);
+router.post("/delete-product", adminController.postDeleteProduct);
 module.exports = router;
